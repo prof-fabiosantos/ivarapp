31d26bd78d7296977ac1fe20f7a6dc2e48e207b3
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2025-03-07 18:57:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def processar_video(uploaded_file):
     st.success("Processamento concluído! ✅")
 
     if irregular_frames:
-        st.write("### 📊 Frames com irregularidades detectadas:")
+        st.write("### Frames com irregularidades detectadas:")
         cols = st.columns(3)
 
         for idx, (frame_path, detected_irregularities) in enumerate(irregular_frames):
@@ -180,27 +180,12 @@ def processar_video(uploaded_file):
 
     os.remove(temp_path)
 
-st.title("🤖 iVAR ⚽")
-
-st.write("""
-Com o iVAR, você resolve de vez aquelas jogadas polêmicas do futebol. Envie o vídeo do lance e nossa IA analisa cada frame usando as Regras Oficiais da FIFA. Tecnologia de ponta para trazer justiça ao seu jogo!
-""")
-
-st.markdown("""
-### 📋 Como funciona
-1. Informe seu e-mail para gerar o link de pagamento;
-2. Realize o pagamento de R$ 5,00;
-3. Envie o vídeo (máximo 10 segundos, formato MP4);
-4. Aguarde enquanto a IA analisa e te envia o resultado.
-""")
-
-st.write("Veja abaixo como a IA analisa cada lance:")
-
-col1, col2, col3 = st.columns([2, 1, 1])
-with col1:
-    st.image("frames.png", caption="Exemplo da análise automática da IA", width=380)
-
-
+st.title("🤖IA Analista de Futebol ⚽🤾‍♂️")
+st.write("Inteligência Artificial que analisa vídeos de jogadas dúvidosas de futebol, visa auxiliar na verificação se a jogada foi ou não irregular.")
+st.write("Envie um vídeo curto no máximo uns 6 segundos. Vídeo deve está no formato mp4")
+st.write("Para realizar uma análise siga os passos: 1)Digite seu e-mail para gerar o link de pagamento;  2)Efetue o pagamento de R$ 10,00;  3)Envie o Vídeo; 4)Realize a análise")
+st.write("Veja na imagem abaixo como a IA realiza a análise.")
+st.image("frames.png", caption="Logo Streamlit", use_container_width=True)
 
 email = st.text_input("Digite seu e-mail para pagar e acessar o analisador:")
 
@@ -209,7 +194,7 @@ if not st.session_state["link_pagamento_clcado"]:
         with st.spinner("Gerando link de pagamento, aguarde..."):
             response = requests.post(
                 "https://apistripe.onrender.com/create-checkout-session/",
-                json={"email": email, "amount": 500}
+                json={"email": email, "amount": 1000}
             )
             if response.status_code == 200:
                 checkout_url = response.json()["checkout_url"]
@@ -256,7 +241,7 @@ else:
 
 if st.session_state.get("acesso_liberado") and not st.session_state["video_processado"]:
     uploaded_file = st.file_uploader("Faça o envio de um vídeo mp4 do lance duvidoso", type=["mp4"])
-    if uploaded_file and st.button("🤖⚽ Analisar a Jogada"):
+    if uploaded_file and st.button("🕴️ Analisar o vídeo"):
         st.session_state["uploaded_file"] = uploaded_file
         st.session_state["processar_video_ativado"] = True
 
