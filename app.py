c731effefa5cb0b7c0a42e310c075f416b2db2c2
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2025-03-07 17:18:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -180,12 +180,27 @@ def processar_video(uploaded_file):
 
     os.remove(temp_path)
 
-st.title("🤖IA Analista de Futebol ⚽🤾‍♂️")
-st.write("Inteligência Artificial que analisa vídeos de jogadas dúvidosas de futebol, visa auxiliar na verificação se a jogada foi ou não irregular.")
-st.write("Envie um vídeo curto no máximo uns 6 segundos. Vídeo deve está no formato mp4")
-st.write("Para realizar uma análise siga os passos: 1)Digite seu e-mail para gerar o link de pagamento;  2)Efetue o pagamento de R$ 10,00;  3)Envie o Vídeo; 4)Realize a análise")
-st.write("Veja na imagem abaixo como a IA realiza a análise.")
-st.image("frames.png", caption="Logo Streamlit", use_container_width=True)
+st.title("🤖 iVAR ⚽🤾‍♂️")
+
+st.write("""
+Com o iVAR, você resolve de vez aquelas jogadas polêmicas do futebol. Envie o vídeo do lance e nossa IA analisa cada frame usando as Regras Oficiais da FIFA. Tecnologia de ponta para trazer justiça ao seu jogo!
+""")
+
+st.markdown("""
+### 📋 Como funciona
+1. Informe seu e-mail para gerar o link de pagamento;
+2. Realize o pagamento de R$ 5,00;
+3. Envie o vídeo (máximo 10 segundos, formato MP4);
+4. Aguarde enquanto a IA analisa e te envia o resultado.
+""")
+
+st.write("Veja abaixo como a IA analisa cada lance:")
+
+col1, col2, col3 = st.columns([2, 1, 1])
+with col1:
+    st.image("frames.png", caption="Exemplo da análise automática da IA", width=400
+
+
 
 email = st.text_input("Digite seu e-mail para pagar e acessar o analisador:")
 
@@ -194,7 +209,7 @@ if not st.session_state["link_pagamento_clcado"]:
         with st.spinner("Gerando link de pagamento, aguarde..."):
             response = requests.post(
                 "https://apistripe.onrender.com/create-checkout-session/",
-                json={"email": email, "amount": 1000}
+                json={"email": email, "amount": 500}
             )
             if response.status_code == 200:
                 checkout_url = response.json()["checkout_url"]
